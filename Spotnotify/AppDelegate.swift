16479af81a77726c39536d945c84f39717e35e0e
cjch0 <chrisjcho3@gmail.com>
2016-04-09 21:06:43 -0700

    Initial Commit

---
+++ Spotnotify/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Spotnotify
+//
+//  Created by Chris Cho on 4/9/16.
+//  Copyright © 2016 Chris Cho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
